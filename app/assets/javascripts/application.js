9727ee78072c3bc66b4b1d5fd9bc9680175bef51
Kenny L. Tran <unique@kennyletran.com>
2014-04-21 18:40:15 -0700

    fixed edit about page. added google analytics

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -282,3 +282,11 @@
 	};
 
 }(window.jQuery || window.$)); 
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-50252719-1', 'sailnote.com');
+ga('send', 'pageview');
